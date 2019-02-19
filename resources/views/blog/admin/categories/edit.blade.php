88cc1e0935827b6bec8eebea74c3d6c6b8dc6bb6
ZSA <m555pa@mail.ru>
2019-02-19 16:08:54 +0600

    Создание категорий

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -2,12 +2,18 @@
 
 @section('content')
     @php /** @var \App\Models\BlogCategory $item */ @endphp
-    <form method="POST" action="{{ route('blog.admin.categories.update', $item->id) }}">
+
+    @if($item->exists)
+        <form method="POST" action="{{ route('blog.admin.categories.update', $item->id) }}">
         @method('PATCH')
+    @else
+        <form method="POST" action="{{ route('blog.admin.categories.store') }}">
+    @endif
         @csrf
+
         <div class="container">
             @php
-                /** @var \Illuminate\Support\ViewErrorBag $errors */
+            /** @var \Illuminate\Support\ViewErrorBag $errors */
             @endphp
             @if($errors->any())
                 <div class="row justify-content-center">
